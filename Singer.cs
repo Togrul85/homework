e187cee9443cd6afbf311f4e8ed51385dfa8b934
unknown <toghrul@code.edu.az>
2022-08-15 03:46:21 +0400

    .

---
+++ Singer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework345
+{
+    class Singer
+    {
+        string _name { get; set; }
+        string _surname { get; set; }
+        int _age { get; set; }
+        
+
+        public string name
+        {
+            get
+            {
+                return _name;
+
+            }
+            set
+            {
+                if (value.Length >= 100)
+                {
+                    value = _name;
+                }
+                if (value.Length >= 100)
+                {
+                    value = _surname;
+
+                }
+                if (value.Length >= 170)
+                {
+                    _ = _age;
+
+                }
+                
+
+            }
+        }
+    }
+}
+
+
+        
+
+    
---
+++ Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework345
+{
+    class Song
+    {
+        public string _name { get; set; }
+        public string _genre { get; set; }
+        public string _singer { get; set; }
+        public string name
+        {
+            get
+            {
+                return _name;
+
+            }
+            set
+            {
+                if (value.Length >= 100)
+                {
+                    value = _name;
+
+                }
+            }
+        }
+
+
+    }
+}
